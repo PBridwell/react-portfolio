9f9b10e01021e0d1192babfcd8c16658e5faa02e
P Bridwell <patbridwell@cox.net>
2020-06-25 14:40:58 -0500

    adds link tags and targets to all project cards

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -16,8 +16,12 @@ function ProjectCard(props) {
 								<h4>
 									<small>Description:</small> {props.info}
 								</h4>
-								<h6 className='text-muted'>GitHub Repo: {props.github}</h6>
-								<h6 className='text-muted'>Deployed app: {props.deployed}</h6>
+								<h6 className='text-muted'>
+									GitHub Repo:<a href='{props.github}'> {props.github}</a>
+								</h6>
+								<h6 className='text-muted'>
+									Deployed app:<a href='{props.deployed}'> {props.deployed}</a>
+								</h6>
 							</div>
 						</div>
 					</div>
