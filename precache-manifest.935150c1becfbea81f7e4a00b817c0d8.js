bfa7eb026bdd479acaaacca4c11cf80f18570d56
P Bridwell <patbridwell@cox.net>
2020-06-25 21:17:52 -0500

    Updates

---
+++ precache-manifest.935150c1becfbea81f7e4a00b817c0d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "222c2dab67fd61a0224e44c5ae9a45c6",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "5c07a33343ff578a8e28",
+    "url": "/react-portfolio/static/css/main.3830cf35.chunk.css"
+  },
+  {
+    "revision": "a0dfa021875599b187ef",
+    "url": "/react-portfolio/static/js/2.56a2c37e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.56a2c37e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c07a33343ff578a8e28",
+    "url": "/react-portfolio/static/js/main.6f0226a5.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "43fb5dba2e73dc3f61be2b537be7c940",
+    "url": "/react-portfolio/static/media/home-background.43fb5dba.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.0092041dada83061dc99bbef97f08e45.js"
+  "/react-portfolio/precache-manifest.935150c1becfbea81f7e4a00b817c0d8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
